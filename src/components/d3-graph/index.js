eeb84e1d4472d40546dffa421ce9ac0172613fab
TarekMahmoud175 <tarek64@live.com>
2022-06-03 01:05:08 +0200

    added axis to graph

--- src/components/d3-graph/index.js
+++ src/components/d3-graph/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Styles from "./graph.module.css";
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 import "https://cdn.skypack.dev/d3-scale@4";
 
-const Graph = () => {
+const Graph = ({ width, height }) => {
   const data = [
-    { day: "22/5/2022", temp: 17 },
-    { day: "23/5/2022", temp: 18 },
-    { day: "24/5/2022", temp: 19 },
-    { day: "25/5/2022", temp: 20 },
-    { day: "26/5/2022", temp: 21 },
-    { day: "27/5/2022", temp: 22 },
-    { day: "28/5/2022", temp: 23 },
+    { day: "22/5", temp: 17 },
+    { day: "23/5", temp: 18 },
+    { day: "24/5", temp: 19 },
+    { day: "25/5", temp: 20 },
+    { day: "26/5", temp: 21 },
+    { day: "27/5", temp: 22 },
+    { day: "28/5", temp: 23 },
   ];
 
   const drawChart = (element) => {
@@ -27,24 +27,50 @@ const Graph = () => {
       .domain([0, Math.max(...temps_array)])
       .nice()
       .range([200, 0]);
-    const container = d3.select(element).classed(Styles.graphContainer, true);
 
-    const bars = container
-      .selectAll(".bar")
+    const svg = d3
+      .select(element)
+      .classed(Styles.graphContainer, true)
+      // .attr("width", 150)
+      // .attr("height", 150)
+      .attr("viewBox", [0, 0, 200, 200]);
+
+    svg
+      .append("g")
+      .attr("fill", "royalblue")
+      .selectAll("rect")
       .data(data)
-      .enter()
-      .append("rect")
-      .classed("bar", true)
+      .join("rect")
       .attr("width", xScale.bandwidth())
-      .attr("height", (element) => 500 - yScale(element.temp))
-      .attr("fill", "var(--clr-secondary)")
+      .attr("height", (element) => yScale(0) - yScale(element.temp))
       .attr("x", (element) => xScale(element.day))
       .attr("y", (element) => yScale(element.temp));
-  };
 
-  //   useEffect(() => {
-  //     drawChart();
-  //   }, []);
+    const xAxis = (g) => {
+      g.attr("transform", "translate(0,200)")
+        .call(d3.axisBottom(xScale).tickFormat((x, i) => data[i].day))
+        .selectAll("g")
+        .selectAll("text")
+        .classed("xAxis", true)
+        .attr("font-size", "4px")
+        .attr("font-Family", "var(--ff-poppinsRegular)")
+        .attr("fill", "var(--clr-black)");
+    };
+
+    svg.append("g").call(xAxis);
+
+    const yAxis = (g) => {
+      g.call(d3.axisLeft(yScale).ticks(null, data.format))
+        .selectAll("g")
+        .selectAll("text")
+        .classed("yAxis", true)
+        .attr("font-size", "4px")
+        .attr("font-Family", "var(--ff-poppinsRegular)")
+        .attr("fill", "var(--clr-black)");
+    };
+
+    svg.append("g").call(yAxis);
+  };
 
   return <svg id="svg-div" ref={drawChart}></svg>;
 };
