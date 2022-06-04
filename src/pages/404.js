431ce9a1debfbc9699573a8ddfa11945b7690e8e
TarekMahmoud175 <tarek64@live.com>
2022-06-04 17:18:40 +0200

    testing heroku

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import Text from "../components/Text";
 const NotFound = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <Text>testing</Text>
+    </div>
+  );
+};
 
 export default NotFound;
